c513f84822cd06eda704840c1fc77dfd74f16f39
shri krishan <142647333+krishan2821@users.noreply.github.com>
2023-12-11 06:20:52 +0000

    knowing about var const let and comments

---
+++ 01_bassic/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 202020
+let accountEmail = "shrikrishan@gmail.com"
+var accountPassword = "1213"
+accountCity = "jaipur"
+
+// accountId = 2323 // not allow
+
+accountEmail = "abcd@gmail.com"
+accountPassword = "54321"
+accountCity = "jodhpur"
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity]);
+
+/* 
+prefer not to use var 
+
+
+*/
