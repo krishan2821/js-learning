3598b7bf6b1425dfae410b9711c800fc57742855
shri krishan <142647333+krishan2821@users.noreply.github.com>
2023-12-11 18:55:24 +0530

    datatype workings

---
+++ 01_bassic/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert( 3 + 3) //we are using nodejs not browser
+
+console.log( 3
+    +
+    3 );  // code must be readabile
+
+
+    let name = "shri"
+    let age = 25;
+    let isLoggedIn = false
+
+    // number => 2 to power 53
+    // bigint
+    // string = ""
+    //Boolean => true/false
+    // null => standalone vale
+    // undefind => value is not assign right now
+     // symbols => unique
+
+     // object
+
+    console.log(typeof age);
